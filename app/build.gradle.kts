f16ce54e1242f867ee98dd51b9ac859551d4d014
pretyflaco <kemal@blink.sv>
2026-02-08 05:39:15 +0300

    fix: Bump versionCode/versionName to 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.blink.pos.companion"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
